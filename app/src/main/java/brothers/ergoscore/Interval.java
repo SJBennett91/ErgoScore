214922558d56153f50a1208e1fc7837c7ce3e32a
SJBennett91 <sjbennett91@hotmail.co.uk>
2015-08-03 20:43:41 +0100

    This one is going out for my home boys. This is for you pablo

---
+++ app/src/main/java/brothers/ergoscore/Interval.java
@@ -0,0 +1,126 @@
+package brothers.ergoscore;
+
+        import android.os.Parcel;
+        import android.os.Parcelable;
+
+/**
+ * Created by mapinguari on 7/24/15.
+ */
+public class Interval implements Parcelable {
+
+    // fields
+    // active work fields
+    private Double workTime;
+    private Double averageWatts;
+    private Integer averageSPM;
+
+    //rest fields
+    private Double restTime;
+
+    //constructors
+
+    public Interval(Double workTime, Double averageWatts, Integer averageSPM, Double restTime) {
+        this.workTime = workTime;
+        this.averageWatts = averageWatts;
+        this.averageSPM = averageSPM;
+        this.restTime = restTime;
+    }
+
+    public Interval(Parcel parcel){
+        readFromParcel(parcel);
+    }
+
+    public String getSplit(){
+        Double a = calculateSplit(this.averageWatts);
+        String str = secondsToSplit(a);
+        return str;
+    }
+
+    public static String calcSplit(Double watts){
+        Double a = calculateSplit(watts);
+        String str = secondsToSplit(a);
+        return str;
+    }
+
+    private static Double calculateSplit(double watts){
+        double a = 2.8/watts;
+        double split = Math.cbrt(a);
+        return split;
+    }
+
+    private static String secondsToSplit(double secondsT){
+        double mins = Math.floor(secondsT / 60);
+        double secs = Math.floor(secondsT % 60);
+        double milli = Math.round((secs % 1) * 10);
+        return (Double.toString(mins) + ":" + Double.toString(secs) + "." + Double.toString(milli));
+    }
+    //public methods
+
+    public Double getWorkTime() {
+        return workTime;
+    }
+
+    public void setWorkTime(Double workTime) {
+        this.workTime = workTime;
+    }
+
+    public Integer getAverageSPM() {
+        return averageSPM;
+    }
+
+    public void setAverageSPM(Integer averageSPM) {
+        this.averageSPM = averageSPM;
+    }
+
+    public Double getAverageWatts() {
+        return averageWatts;
+    }
+
+    public void setAverageWatts(Double averageWatts) {
+        this.averageWatts = averageWatts;
+    }
+
+    public Double getRestTime() {
+        return restTime;
+    }
+
+    public void setRestTime(Double restTime) {
+        this.restTime = restTime;
+    }
+
+
+    //Parcelable code
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeDouble(workTime);
+        dest.writeDouble(averageWatts);
+        dest.writeInt(averageSPM);
+        dest.writeDouble(restTime);
+    }
+
+    public void readFromParcel(Parcel in){
+        workTime = in.readDouble();
+        averageWatts = in.readDouble();
+        averageSPM = in.readInt();
+        restTime = in.readDouble();
+    }
+
+    public static final Creator<Interval> CREATOR = new Parcelable.Creator<Interval>(){
+        @Override
+        public Interval createFromParcel(Parcel source) {
+            return new Interval(source);
+        }
+
+        @Override
+        public Interval[] newArray(int size) {
+            return new Interval[size];
+        }
+    };
+}
---
+++ app/src/main/java/brothers/ergoscore/Workout.java
@@ -0,0 +1,112 @@
+package brothers.ergoscore;
+
+        import android.os.Parcel;
+        import android.os.Parcelable;
+
+        import java.util.GregorianCalendar;
+        import java.util.List;
+
+/**
+ * Created by mapinguari on 7/24/15.
+ */
+public class Workout implements Parcelable {
+
+    //fields
+    private GregorianCalendar workoutTime;
+    private Double averageWatts;
+    private Double totalTime;
+    private Integer averageSPM;
+    private List<Interval> intervalList;
+
+    //constructors
+    public Workout(List<Interval> intervalList, Integer averageSPM, Double averageWatts, Double totalTime, GregorianCalendar workoutTime) {
+        this.intervalList = intervalList;
+        this.averageSPM = averageSPM;
+        this.averageWatts = averageWatts;
+        this.totalTime = totalTime;
+        this.workoutTime = workoutTime;
+    }
+
+    public Workout(Parcel workout){
+        this.readFromParcel(workout);
+    }
+
+    //methods
+    public GregorianCalendar getWorkoutTime() {
+        return workoutTime;
+    }
+
+    public void setWorkoutTime(GregorianCalendar workoutTime) {
+        this.workoutTime = workoutTime;
+    }
+
+    public Integer getAverageSPM() {
+        return averageSPM;
+    }
+
+    public void setAverageSPM(Integer averageSPM) {
+        this.averageSPM = averageSPM;
+    }
+
+    public Double getAverageWatts() {
+        return averageWatts;
+    }
+
+    public void setAverageWatts(Double averageWatts) {
+        this.averageWatts = averageWatts;
+    }
+
+    public Double getTotalTime() {
+        return totalTime;
+    }
+
+    public void setTotalTime(Double totalTime) {
+        this.totalTime = totalTime;
+    }
+
+    public List<Interval> getIntervalList() {
+        return intervalList;
+    }
+
+    //TODO: write verification function. i.e one that checks that the header workout data matches the intervals workout data
+
+    // Parcelable code here
+
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeSerializable(workoutTime);
+        dest.writeDouble(averageWatts);
+        dest.writeDouble(totalTime);
+        dest.writeInt(averageSPM);
+        dest.writeList(intervalList);
+    }
+
+    public void readFromParcel(Parcel in){
+        workoutTime = (GregorianCalendar) in.readSerializable();
+        averageWatts = in.readDouble();
+        totalTime = in.readDouble();
+        averageSPM = in.readInt();
+        //TODO: Not sure here if this class loader (null) is acceptable
+        in.readList(this.intervalList,Interval.class.getClassLoader());
+    }
+
+    public static final Creator<Workout> CREATOR = new Parcelable.Creator<Workout>(){
+        @Override
+        public Workout createFromParcel(Parcel source) {
+            return new Workout(source);
+        }
+
+        @Override
+        public Workout[] newArray(int size) {
+            return new Workout[size];
+        }
+    };
+
+
+}
